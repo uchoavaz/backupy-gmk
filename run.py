b92068cb7213bd44fd924d802d61d517acab7ffe
joao uchoa <joao.victor@genomika.com.br>
2017-06-22 16:53:18 +0000

    Update run.py

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ BKP_CONFIG = {
     'host_machine': '172.16.225.9',
     'db_password': 'g3n3t1c@',
     'port': '5432',
-    'local_destiny_folder': '/rancher/rosalind/Backups/Bancos/',
+    'local_destiny_folder': '/rancher/',
     'server_mount_folder': '/rosalind/Backups/Bancos/',
     'DB_IGNORED': [
         'template1',
