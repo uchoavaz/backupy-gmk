c4d014e3207be628d3208898e3f9aeff52ca3dda
joao uchoa <joao.victor@genomika.com.br>
2017-09-13 17:22:46 +0000

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,11 +4,8 @@
 echo "listen_addresses = '*'" >> /etc/postgresql/9.6/main/postgresql.conf
 service postgresql restart
 
-git clone git@gitlab.com:genomika/backupy.git
-cd backupy
-
 git checkout $backupy_branch
 
-sh crontab.sh
+/www/var/backupy/crontab.sh
 
 cron -f
