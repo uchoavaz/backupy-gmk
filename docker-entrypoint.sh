ad76ebaf6dacb83f9ac1169bc6d0b6fae05b9131
joao uchoa <joao.victor@genomika.com.br>
2018-08-02 19:24:10 +0000

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,6 @@ cd /var/www/backupy
 pip3 install -r requirements.txt
 
 crontab -r
-(crontab -l 2>/dev/null; echo "50 23 * * * python3 /var/www/backupy/executor.py") | crontab -
+(crontab -l 2>/dev/null; echo "00 23 * * * python3 /var/www/backupy/executor.py") | crontab -
 
 cron -f
